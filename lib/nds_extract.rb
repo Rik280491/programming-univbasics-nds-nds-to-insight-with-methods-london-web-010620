52772fcdc848521b58bd0f601f54d7d0afc1a8a9
Rikesh Balakrishnan <rik280491@googlemail.com>
2019-12-23 15:58:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -16,23 +16,4 @@ def gross_for_director(director_data)
 end
   
   
-  
-  
-  result = {}
-  director_index = 0
 
-  while director_index < nds.length do
-    director_name = nds[director_index][:name]
-    totals = 0
-    movie_index = 0
-
-    while movie_index < nds[director_index][:movies].length do
-      totals += gross_for_director(director_data)
-      movie_index += 1
-    end
-
-    director_index += 1
-  end
-
-  totals
-end
